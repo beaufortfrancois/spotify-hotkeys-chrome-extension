9ac28e55ab7f60f14c551c18ef9d75c3568f617b
Marco Simone <marcosimone@umail.ucsb.edu>
2015-05-25 20:25:48 -0700

    updated for new spotify layout

--- src/background.js
+++ src/background.js
@@ -1,12 +1,12 @@
 function onCommand(command) {
-  chrome.tabs.query({url: 'https://play.spotify.com/*'}, function(tabs) {
+  chrome.tabs.query({url: 'https://player.spotify.com/*'}, function(tabs) {
 
     // Open a spotify tab if one does not exist yet.
     if (tabs.length === 0) {
-      chrome.tabs.create({url: 'https://play.spotify.com'});
+      chrome.tabs.create({url: 'https://player.spotify.com'});
     }
-
-    var code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+      
+    var code = "document.getElementById('main').contentDocument.querySelector('#" + command +  "').click()";
     // Apply command on all spotify tabs.
     for (var tab of tabs) {
       chrome.tabs.executeScript(tab.id, {code: code});
