b42d7d5d668f24d9fd173cd9f28e386483cef87e
Marco Simone <marcojsimone@gmail.com>
2015-05-27 01:53:17 -0700

    player support, beginning play support

--- src/background.js
+++ src/background.js
@@ -1,20 +1,29 @@
+
 function onCommand(command) {
-  chrome.tabs.query({url: 'https://player.spotify.com/*'}, function(tabs) {
-
-    // Open a spotify tab if one does not exist yet.
-    if (tabs.length === 0) {
-      chrome.tabs.create({url: 'https://player.spotify.com'});
-    }
-      
-    var code = "document.getElementById('main').contentDocument.querySelector('#" + command +  "').click()";
-    // Apply command on all spotify tabs.
-    for (var tab of tabs) {
-      chrome.tabs.executeScript(tab.id, {code: code});
-    }
-
-    // Unload background page as soon as we're done.
-    window.close();
-  });
+    chrome.tabs.query({url: 'https://*.spotify.com/*'}, function(tabs) {
+	
+	// Open a spotify tab if one does not exist yet. (will auto redirect if play)
+	if (tabs.length === 0) {
+	    chrome.tabs.create({url: 'https://player.spotify.com'});
+	}
+	
+	var code_player = "document.getElementById('main').contentDocument.querySelector('#" + command +  "').click()";
+	var code_play="document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+	
+	// Apply command on all spotify tabs.
+	var reg_player = new RegExp("/.+player[.]spotify[.]com/.+/");
+	var reg_play = new RegExp("/.+play[.]spotify[.]com/.+/");
+	
+	for (var tab of tabs) {
+	    if(reg_player.test(tab.url))
+		chrome.tabs.executeScript(tab.id, {code: code_player});
+	    if(reg_play.test(tab.url))
+		chrome.tabs.executeScript(tab.id, {code: code_play});
+	}
+	
+	// Unload background page as soon as we're done.
+	window.close();
+    });
 };
 
 chrome.commands.onCommand.addListener(onCommand);
