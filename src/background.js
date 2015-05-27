83168b1e70c53d74642937887673a91c6366bf01
Marco Simone <marcojsimone@gmail.com>
2015-05-27 02:55:13 -0700

    added support for both layouts

--- src/background.js
+++ src/background.js
@@ -2,23 +2,31 @@
 function onCommand(command) {
     chrome.tabs.query({url: 'https://*.spotify.com/*'}, function(tabs) {
 	
-	// Open a spotify tab if one does not exist yet. (will auto redirect if play)
+	// Open a spotify tab if one does not exist yet. 
 	if (tabs.length === 0) {
 	    chrome.tabs.create({url: 'https://player.spotify.com'});
 	}
 	
 	var code_player = "document.getElementById('main').contentDocument.querySelector('#" + command +  "').click()";
-	var code_play="document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+	if(command=="play"){
+		var code_play="document.getElementById('app-player').contentDocument.getElementById('play-pause').click()";
+	}else{
+		var code_play="document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+	}
+	
+	
+	var reg_player = new RegExp("/.+player[.]spotify[.]com/*/");
+	var reg_play = new RegExp("/.+play[.]spotify[.]com/*/");
 	
 	// Apply command on all spotify tabs.
-	var reg_player = new RegExp("/.+player[.]spotify[.]com/.+/");
-	var reg_play = new RegExp("/.+play[.]spotify[.]com/.+/");
-	
 	for (var tab of tabs) {
+		
 	    if(reg_player.test(tab.url))
-		chrome.tabs.executeScript(tab.id, {code: code_player});
+			chrome.tabs.executeScript(tab.id, {code: code_player});
+		
 	    if(reg_play.test(tab.url))
-		chrome.tabs.executeScript(tab.id, {code: code_play});
+			chrome.tabs.executeScript(tab.id, {code: code_play});
+		
 	}
 	
 	// Unload background page as soon as we're done.
