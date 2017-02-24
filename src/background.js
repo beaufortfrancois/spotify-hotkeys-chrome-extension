a372fc50eb2e283c0a31d30a869feb46332a159d
Francois Beaufort <beaufort.francois@gmail.com>
2017-02-24 11:12:32 +0100

    Handle open.spotify.com web player

--- src/background.js
+++ src/background.js
@@ -1,14 +1,30 @@
 function onCommand(command) {
-  chrome.tabs.query({url: 'https://play.spotify.com/*'}, function(tabs) {
+  chrome.tabs.query({url: 'https://*.spotify.com/*'}, function(tabs) {
 
     // Open a spotify tab if one does not exist yet.
     if (tabs.length === 0) {
       chrome.tabs.create({url: 'https://play.spotify.com'});
     }
 
-    var code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
     // Apply command on all spotify tabs.
     for (var tab of tabs) {
+
+      if (tab.url.startsWith('https://play.spotify.com')) {
+        var code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+      } else if (tab.url.startsWith('https://open.spotify.com')) {
+        var buttonClass = '';
+        switch (command) {
+          case 'next': buttonClass = 'spoticon-skip-forward-24'; break;
+          case 'play-pause': buttonClass = 'btn-green'; break;
+          case 'previous': buttonClass = 'spoticon-skip-back-24'; break;
+          case 'shuffle': buttonClass = 'spoticon-shuffle-16'; break;
+          case 'repeat': buttonClass = 'spoticon-repeat-16'; break;
+        }
+        if (!buttonClass.length) {
+          continue;
+        }
+        var code = "document.querySelector('." + buttonClass + "').click()";
+      }
       chrome.tabs.executeScript(tab.id, {code: code});
     }
 
