55d6c79a0de7a0c3ac427495b52aca6dba70f915
Elmardus <elmar@dreissen.eu>
2017-03-30 12:16:50 +0200

    Fix for new button names (open.spotify update)

--- src/background.js
+++ src/background.js
@@ -14,11 +14,11 @@ function onCommand(command) {
         code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
       } else if (tab.url.startsWith('https://open.spotify.com')) {
         switch (command) {
-          case 'next': code = 'document.querySelector(".spoticon-skip-forward-24").click()'; break;
-          case 'previous': code = 'document.querySelector(".spoticon-skip-back-24").click()'; break;
+          case 'next': code = 'document.querySelector(".spoticon-skip-forward-16").click()'; break;
+          case 'previous': code = 'document.querySelector(".spoticon-skip-back-16").click()'; break;
           case 'shuffle': code = 'document.querySelector(".spoticon-shuffle-16").click()'; break;
           case 'repeat': code = 'document.querySelector(".spoticon-repeat-16").click()'; break;
-          case 'play-pause': code = '(document.querySelector(".spoticon-play-32") || document.querySelector(".spoticon-pause-32")).click()'; break;
+          case 'play-pause': code = '(document.querySelector(".spoticon-play-16") || document.querySelector(".spoticon-pause-16")).click()'; break;
         }
       }
       if (code.length) {
