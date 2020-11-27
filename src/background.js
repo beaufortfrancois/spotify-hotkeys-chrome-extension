30b59b10c8be4faa6649bf1a98948527d156b116
Aaron Pinto <AaronPinto@users.noreply.github.com>
2020-11-27 16:05:45 -0500

    updated commands

--- src/background.js
+++ src/background.js
@@ -1,35 +1,37 @@
 function onCommand(command) {
-  chrome.tabs.query({url: 'https://*.spotify.com/*'}, function(tabs) {
+	chrome.tabs.query({url: 'https://*.spotify.com/*'}, tabs => {
+		// Open a spotify tab if one does not exist yet.
+		if (tabs.length === 0) chrome.tabs.create({url: 'https://open.spotify.com/collection/tracks'});
 
-    // Open a spotify tab if one does not exist yet.
-    if (tabs.length === 0) {
-      chrome.tabs.create({url: 'https://open.spotify.com'});
-    }
-
-    // Apply command on all spotify tabs.
-    for (var tab of tabs) {
-
-      var code = '';
-      if (tab.url.startsWith('https://play.spotify.com')) {
-        code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
-      } else if (tab.url.startsWith('https://open.spotify.com')) {
-        switch (command) {
-          case 'next': code = 'document.querySelector(".spoticon-skip-forward-16").click()'; break;
-          case 'previous': code = 'document.querySelector(".spoticon-skip-back-16").click()'; break;
-          case 'shuffle': code = 'document.querySelector(".spoticon-shuffle-16").click()'; break;
-          case 'repeat': code = 'document.querySelector(".spoticon-repeat-16").click()'; break;
-          case 'track-add': code = '(document.querySelector(".spoticon-heart-16") || document.querySelector(".spoticon-heart-active-16")).click()'; break;
-          case 'play-pause': code = '(document.querySelector(".spoticon-play-16") || document.querySelector(".spoticon-pause-16")).click()'; break;
-        }
-      }
-      if (code.length) {
-        chrome.tabs.executeScript(tab.id, {code: code});
-      }
-    }
-
-    // Unload background page as soon as we're done.
-    window.close();
-  });
-};
+		// Apply command on all spotify tabs.
+		for (let tab of tabs) {
+			let code = '';
+			if (tab.url.startsWith('https://open.spotify.com'))
+				switch (command) {
+					case 'next':
+						code = 'document.querySelector(".spoticon-skip-forward-16").click()';
+						break;
+					case 'previous':
+						code = 'document.querySelector("[title=\\"Previous\\"]").click()';
+						break;
+					case 'shuffle':
+						code = '(document.querySelector("[title=\\"Enable shuffle\\"]")?.click() || document.querySelector("[title=\\"Disable shuffle\\"]")?.click())'
+						break;
+					case 'repeat':
+						code = '(document.querySelector(".spoticon-repeat-16")?.click() || document.querySelector(".spoticon-repeatonce-16")?.click())'
+						break;
+					case 'track-add':
+						code = '(document.querySelector("[title=\\"Save to Your Library\\"]")?.click() || document.querySelector("[title=\\"Remove from Your Library\\"]")?.click())';
+						break;
+					case 'play-pause':
+						code = '(document.querySelector("[title=\\"Play\\"]")?.click() || document.querySelector("[title=\\"Pause\\"]")?.click())';
+						break;
+				}
+			if (code.length) chrome.tabs.executeScript(tab.id, {code: code});
+		}
+		// Unload background page as soon as we're done.
+		window.close();
+	});
+}
 
 chrome.commands.onCommand.addListener(onCommand);
