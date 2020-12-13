d5686a909259b9feb5ec4736c3fd81f72c6043e1
Aaron Pinto <AaronPinto@users.noreply.github.com>
2020-12-12 20:30:05 -0500

    update command codes and only run on 1 tab
    I've updated the command codes to use the button class names, and I've updated the code to only run on 1 spotify tab, because if you have multiple web player tabs open, spotify syncs them automatically.

--- src/background.js
+++ src/background.js
@@ -3,31 +3,37 @@ function onCommand(command) {
 		// Open a spotify tab if one does not exist yet.
 		if (tabs.length === 0) chrome.tabs.create({url: 'https://open.spotify.com/collection/tracks'});
 
-		// Apply command on all spotify tabs.
 		for (let tab of tabs) {
 			let code = '';
-			if (tab.url.startsWith('https://open.spotify.com'))
+
+			if (tab.url.startsWith('https://open.spotify.com')) {
 				switch (command) {
 					case 'next':
-						code = 'document.querySelector("[title=\\"Next\\"]").click()';
+						code = 'document.querySelector(".bf01b0d913b6bfffea0d4ffd7393c4af-scss").click()';
 						break;
 					case 'previous':
-						code = 'document.querySelector("[title=\\"Previous\\"]").click()';
+						code = 'document.querySelector(".bc13c597ccee51a09ec60253c3c51c75-scss").click()';
 						break;
 					case 'shuffle':
-						code = '(document.querySelector("[title=\\"Enable shuffle\\"]")?.click() || document.querySelector("[title=\\"Disable shuffle\\"]")?.click())'
+						code = 'document.querySelector("._39234eb5c173f8b6de80ed73820b1be8-scss").click()';
 						break;
 					case 'repeat':
-						code = '(document.querySelector(".spoticon-repeat-16")?.click() || document.querySelector(".spoticon-repeatonce-16")?.click())'
+						code = 'document.querySelector(".ebfd411a126f1e7bea6133f21b4ef88e-scss").click()';
 						break;
 					case 'track-add':
-						code = '(document.querySelector("[title=\\"Save to Your Library\\"]")?.click() || document.querySelector("[title=\\"Remove from Your Library\\"]")?.click())';
+						code = 'document.querySelector("._07bed3a434fa59aa1852a431bf2e19cb-scss").click()';
 						break;
 					case 'play-pause':
-						code = '(document.querySelector("[title=\\"Play\\"]")?.click() || document.querySelector("[title=\\"Pause\\"]")?.click())';
+						code = 'document.querySelector("._82ba3fb528bb730b297a91f46acd37a3-scss").click()';
 						break;
 				}
-			if (code.length) chrome.tabs.executeScript(tab.id, {code: code});
+			}
+
+			// Apply command on only 1 spotify tab.
+			if (code.length) {
+				chrome.tabs.executeScript(tab.id, {code: code});
+				break;
+			}
 		}
 		// Unload background page as soon as we're done.
 		window.close();
